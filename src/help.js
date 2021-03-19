16c89856eb56416a4bf402337a90bfda4dc979ee
pereira666 <80645303+pereira666@users.noreply.github.com>
2021-03-19 11:15:55 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,57 @@
+const help = (prefix) => {
+	return `╔─━━━━━━░ 🐊𝙇𝙊𝙎𝙀𝙍 𝘽𝙊𝙏 🐊 ━━━━━━─╗ 
+	
+• _*🤠 𝐶𝑅𝐼𝐴𝐷𝑂𝑅: WA.me/558499034946*_
+• _*❗𝐴𝑉𝐼𝑆𝑂: EM CASOS DE BUG DO BOT CHAME O CRIADOR!, POIS O BOT AINDA ESTA EM DESENVOLVIMENTO E PODE OCORRER BUGS❗*_
+
+╚─━━━━━━━━━━░   •   ━━━━━━━━━━━━─╝
+
+• ───━━━━─ ғɪɢᴜʀɪɴʜᴀs/sᴛɪᴄᴋᴇʀs ─━━━━─── •
+	
+✱ *${prefix}info*
+✱ *${prefix}sticker* ou *${prefix}stiker*
+✱ *${prefix}sticker nobg* or *${prefix}stiker nobg*
+✱ *${prefix}toimg*
+✱ *${prefix}tsticker* or *${prefix}tstiker*
+
+• ───━━━━─ ᴍᴇᴍᴇ ─━━━━─── •
+
+✱ *${prefix}meme*
+
+• ───━━━━─ ᴅᴏᴡɴʟᴏᴀᴅs / sᴛᴀʟᴋᴇʀ ─━━━━─── •
+
+✱ *${prefix}yt2mp3*
+✱ *${prefix}tiktok*
+✱ *${prefix}tiktokstalk*
+✱ *${prefix}ytsearch*
+
+• ───━━━━─ ᴀᴜᴅɪᴏ ─━━━━─── •
+
+✱ *{${prefix}gtts*
+
+• ───━━━━─ ᴏᴜᴛʀᴏs ─━━━━─── •
+
+✱ *${prefix}tp*
+✱ *${prefix}ep*
+✱ *${prefix}tahta*
+✱ *${prefix}loli*
+✱ *${prefix}nsfwloli*
+✱ *${prefix}url2img*
+✱ *${prefix}simi*
+✱ *${prefix}ocr*
+✱ *${prefix}wait*
+✱ *${prefix}setprefix*
+
+• ───━━━━─ ɢʀᴜᴘᴏs ─━━━━─── •
+
+✱ *${prefix}add*
+✱ *${prefix}kick*
+✱ *${prefix}promote*
+✱ *${prefix}demote*
+✱ *${prefix}linkgroup*
+✱ *${prefix}leave*
+✱ *${prefix}tagall*
+✱ *${prefix}simih*
+}
+
+exports.help = help
